53e9adb7262b06b2f577a2775c07d5f3726705d4
dragonkill3r <122841379+dragonkill3r@users.noreply.github.com>
2023-02-09 18:20:32 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Deliverable_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 25;
+            int[] fibonacciSequence = new int[size];
+            fibonacciSequence[0] = 0;
+            fibonacciSequence[1] = 1;
+
+            for (int i = 2; i < size; i++)
+            {
+                fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Fibonacci number ({0}) = {1}", i, fibonacciSequence[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
